4a05af5b99732f38a1f043d716d0f1aeb9e3a4c9
Daniel Mican <danielmican@Daniels-MacBook-Pro.local>
2024-06-30 11:49:59 -0400

    generate CLI command

---
+++ internal/cmd/generate.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/turbolytics/flake/pkg/flake"
+)
+
+var (
+	regionID  uint16
+	machineID uint16
+	count     int
+)
+
+var generateCmd = &cobra.Command{
+	Use:   "generate",
+	Short: "Generate a new 128-bit Flake ID",
+	Long:  `Generates one or more 128-bit Flake IDs using the specified region and machine IDs.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fg := flake.NewGenerator(regionID, machineID)
+		for i := 0; i < count; i++ {
+			id, err := fg.GenerateFlakeID()
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(id.String())
+		}
+	},
+}
+
+func init() {
+	// Add flags for the generate command
+	generateCmd.Flags().Uint16VarP(&regionID, "region", "r", 1, "Region ID")
+	generateCmd.Flags().Uint16VarP(&machineID, "machine", "m", 1, "Machine ID")
+	generateCmd.Flags().IntVarP(&count, "count", "c", 1, "Number of IDs to generate")
+
+	// Add generate command to the root command
+	rootCmd.AddCommand(generateCmd)
+}
